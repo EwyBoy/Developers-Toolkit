c913bcd64388907c181732bc3ba703c3e10407f9
Eivind Norling <EwyBoy@gmail.com>
2021-09-01 16:32:11 +0200

    Initial commit

---
+++ src/main/java/com/ewyboy/template/Template.java
@@ -0,0 +1,16 @@
+package com.ewyboy.template;
+
+import net.minecraftforge.fml.common.Mod;
+
+import static com.ewyboy.template.Template.MOD_ID;
+
+@Mod(MOD_ID)
+public class Template {
+
+    public static final String MOD_ID = "template";
+
+    public Template() {
+
+    }
+
+}
